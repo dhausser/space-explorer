7fc84dff6ff95c9efb6fccd47a353f1dfca37b97
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-04-09 23:16:11 +0200

    complete app

--- client/src/index.js
+++ client/src/index.js
@@ -21,6 +21,8 @@ const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     headers: {
       authorization: localStorage.getItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
     },
   }),
   resolvers,
