650fd4aedde9292279cc7c85878782741c3b864e
Davy <24432220+dhausser@users.noreply.github.com>
2019-05-06 09:17:12 +0200

    minor

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -31,11 +31,9 @@ module.exports = {
   },
   Mission: {
     // make sure the default size is 'large' in case user doesn't specify
-    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
-      return size === 'SMALL'
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => size === 'SMALL'
         ? mission.missionPatchSmall
-        : mission.missionPatchLarge;
-    },
+        : mission.missionPatchLarge,
   },
   Launch: {
     isBooked: async (launch, _, { dataSources }) =>
@@ -45,9 +43,9 @@ module.exports = {
     trips: async (_, __, { dataSources }) => {
       // get ids of launches by user
       const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
-  
+
       if (!launchIds.length) return [];
-  
+
       // look up those launches by their ids
       return (
         dataSources.launchAPI.getLaunchesByIds({
@@ -66,27 +64,27 @@ module.exports = {
       const launches = await dataSources.launchAPI.getLaunchesByIds({
         launchIds,
       });
-  
+
       return {
         success: results && results.length === launchIds.length,
         message:
           results.length === launchIds.length
             ? 'trips booked successfully'
             : `the following launches couldn't be booked: ${launchIds.filter(
-                id => !results.includes(id),
+                id => !results.includes(id)
               )}`,
         launches,
       };
     },
     cancelTrip: async (_, { launchId }, { dataSources }) => {
       const result = dataSources.userAPI.cancelTrip({ launchId });
-  
+
       if (!result)
         return {
           success: false,
           message: 'failed to cancel trip',
         };
-  
+
       const launch = await dataSources.launchAPI.getLaunchById({ launchId });
       return {
         success: true,
